58ef6a18ddb3cd08f6a5576e27828d24c83f0f5c
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-06-29 13:34:32 -0400

    add repo init script

---
+++ init-gsa-repo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl -sO https://raw.githubusercontent.com/GSA/open-source-policy/master/LICENSE.md
+curl -sO https://raw.githubusercontent.com/GSA/open-source-policy/master/CONTRIBUTING.md
+
+echo "Added default LICENSE.md and CONTRIBUTING.md files."
